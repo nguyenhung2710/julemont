a5c1d9170df74c5f54ecad667386a656f236e7f3
nguyenhung2710 <72492981+nguyenhung2710@users.noreply.github.com>
2021-03-04 16:56:47 +0700

    ver 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,40 @@
 $(document).ready(function () {
-    var galleryThumbs = new Swiper(".product-title__slide", {
+    var galleryTitle = new Swiper(".product-description", {
         direction: "vertical",
-        spaceBetween: 10,
-        height: 40,
+        spaceBetween: 30,
+        height: 150,
+        loop: true,
         //slidesPerView: 1,
         freeMode: false,
         watchSlidesVisibility: true,
         watchSlidesProgress: true,
-        
+    });
+    var galleryThumbs = new Swiper(".product-title__slide", {
+        direction: "vertical",
+        spaceBetween: 10,
+        height: 40,
+        loop: true,
+        //slidesPerView: 1,
+        freeMode: false,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        thumbs: {
+            swiper: galleryTitle,
+        },
     });
     var galleryTop = new Swiper(".swiper-container", {
         direction: "horizontal",
         spaceBetween: 10,
         loop: true,
+        grabCursor: true,
+        preventClicks: true,
         navigation: {
             nextEl: '.btn-next',
             prevEl: '.btn-prev',
         },
         thumbs: {
-            swiper: galleryThumbs
-        }
+            swiper: galleryThumbs,
+            multipleActiveThumbs: true,
+        },
     });
 });
---
+++ assets/js/dist/app.dev.js
@@ -0,0 +1,42 @@
+"use strict";
+
+$(document).ready(function () {
+  var galleryTitle = new Swiper(".product-description", {
+    direction: "vertical",
+    spaceBetween: 30,
+    height: 150,
+    loop: true,
+    //slidesPerView: 1,
+    freeMode: false,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true
+  });
+  var galleryThumbs = new Swiper(".product-title__slide", {
+    direction: "vertical",
+    spaceBetween: 10,
+    height: 40,
+    loop: true,
+    //slidesPerView: 1,
+    freeMode: false,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    thumbs: {
+      swiper: galleryTitle
+    }
+  });
+  var galleryTop = new Swiper(".swiper-container", {
+    direction: "horizontal",
+    spaceBetween: 10,
+    loop: true,
+    grabCursor: true,
+    preventClicks: true,
+    navigation: {
+      nextEl: '.btn-next',
+      prevEl: '.btn-prev'
+    },
+    thumbs: {
+      swiper: galleryThumbs,
+      multipleActiveThumbs: true
+    }
+  });
+});
